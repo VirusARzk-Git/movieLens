8f6d200e8059f5a9bfb9509c619f6aef1b3b5e86
VirusARzk-Git <aditya.21ug1001@iiitranchi.ac.in>
2024-09-18 17:38:39 +0530

    commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_movie_ticket_booking_app/screens/home_page_screen.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.light,
+    ),
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        fontFamily: "Open-Sans",
+      ),
+      home: HomePageScreen(key: Key('home')),
+    );
+  }
+}
